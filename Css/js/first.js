b82ab9c8fef5139f7a1cc4d2de01c3ce3a217eb7
rutuja <rutujaingale0230@example.com>
2021-11-25 17:28:09 +0530

    first commit

---
+++ Css/js/first.js
@@ -0,0 +1,13 @@
+function myFunction(a, b) {
+    
+    return a * b;
+  }
+  function ValidateEmail(mail) 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
+  {
+    return (true)
+  }
+    alert("You have entered an invalid email address!")
+    return (false)
+} 
